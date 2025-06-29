c99bacaebd7a82294091062271f34c415c767e60
Sekhar <192883287+chandra-samal@users.noreply.github.com>
2025-06-29 13:03:55 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <raylib.h>
+#include <raymath.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    const int width = 640;
+    const int height = 640;
+    InitWindow(width, height, "game");
+
+
+    CloseWindow();
+
+    
+    return 0;
+}
