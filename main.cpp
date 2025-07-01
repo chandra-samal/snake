6b3576cc9b387cb8bfdb8cc33044b5f556cb04c9
Sekhar <192883287+chandra-samal@users.noreply.github.com>
2025-07-02 00:56:14 +0530

    snake game

--- main.cpp
+++ main.cpp
@@ -1,150 +1,184 @@
 #include <raylib.h>
-#include <raymath.h>
 #include <iostream>
-// #include <rshapes.h>
+#include <raymath.h>
+#include <deque>
+
 
 using namespace std;
 
-int player_score = 0;
-int cpu_score = 0;
+Color Light_Green = Color{173, 204, 96, 255};
+float cellSize = 30;
+int cellCount = 30;
+// int offset = 75;
 
-Color Green = Color{38, 185, 154, 255};
-Color Dark_Green = Color{20, 160, 133, 255};
-Color Light_Green = Color{129, 204, 184, 255};
-Color Yellow = Color{243, 213, 91, 255};
+double lastUpdateTime = 0;
 
+bool eventTrigger(double interval){
+    double currentTime = GetTime();
 
-class Ball{
+    if (currentTime-lastUpdateTime>=interval){
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+bool elementInDequeu(Vector2 element, deque<Vector2> deq){
+    for (unsigned int i = 0; i<deq.size(); i++){
+        if (Vector2Equals(deq[i], element)) return true;
+    }
+    return false;
+}
+
+class Snake{
     public:
-    float x, y;
-    int speed_x, speed_y;
-    int radius;
-    
+    deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+    Vector2 direction {1, 0};
+
+    bool addSegment = false;
+
     void Draw(){
-        DrawCircle(x, y, radius, Yellow);
+        for (unsigned i = 0; i<body.size(); i++){
+            float x = body[i].x;
+            float y = body[i].y;
+            DrawRectangleRounded(Rectangle{x * cellSize, y*cellSize, cellSize, cellSize}, 0.5, 6, DARKGRAY);
+        }
     }
 
     void Update(){
-        x += speed_x;
-        y += speed_y;
-
-        if (y+radius >= GetScreenHeight() || y - radius <= 0) 
-        speed_y *= -1;
-
-        if (x+radius >= GetScreenWidth()){
-            cpu_score++;
-            Reset();
+        body.push_front(body[0] + direction);
+        if (addSegment){
+            addSegment = false;
         }
-        if (x - radius <= 0) {
-            player_score++;
-            Reset();
+        else {
+            body.pop_back();
         }
     }
 
     void Reset(){
-        x = GetScreenWidth()/2;
-        y = GetScreenHeight()/2;
-
-        int speedChoices[2] = {-1, 1};
-        speed_x *= speedChoices[GetRandomValue(0, 1)];
-        speed_y *= speedChoices[GetRandomValue(0, 1)];
+        body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+        direction = {1, 0};
     }
 };
 
-class Paddle{
-    protected:
-    void LimitMovement(){
-        if (y<=0) y = 0;
-        if (y>=GetScreenHeight() - height) y = GetScreenHeight() - height;
+class Pellet{
+    public:
+    Vector2 position;
+    Texture2D texture;
+
+    Pellet(deque<Vector2> snakeBody){
+        texture = LoadTexture("graphics/food.png");
+        position = GenerateRandomPos(snakeBody);
     }
 
-    public:
-    float x, y;
-    float width, height;
-    int speed;
+    ~Pellet(){
+        UnloadTexture(texture);
+    }
 
     void Draw(){
-        DrawRectangleRounded(Rectangle{x, y, width, height}, 0.8, 0, WHITE);
+        DrawTexture(texture, position.x*cellSize, position.y*cellSize, WHITE);
+    }
+
+    Vector2 GenerateRandomCell(){
+        float x = GetRandomValue(0, cellCount-1);
+        float y = GetRandomValue(0, cellCount-1);
+        return Vector2{x, y};
+    }
+
+    Vector2 GenerateRandomPos(deque<Vector2> snakeBody){
+        Vector2 position = GenerateRandomCell();
+        while (elementInDequeu(position, snakeBody)){
+            position = GenerateRandomCell();
+        }
+        return position;
+    }
+};
+
+class Game{
+public:
+    Snake snake;
+    Pellet food = Pellet(snake.body);
+    bool running = true;
+
+    void Draw(){
+        food.Draw();
+        snake.Draw();
     }
 
     void Update(){
-        if(IsKeyDown(KEY_UP)) y -= speed;
-        if(IsKeyDown(KEY_DOWN)) y += speed; 
-        LimitMovement();
+        if (running){
+            snake.Update();
+            checkCollision();
+            checkCollisionWithBorder();
+            checkCollisionWithTail();
+        }
+    }
+
+    void checkCollision(){
+        if(Vector2Equals(snake.body[0], food.position)){
+            food.position = food.GenerateRandomPos(snake.body);
+            snake.addSegment = true;
+        }
+    }
+
+    void checkCollisionWithBorder(){
+        if (snake.body[0].x == cellCount || snake.body[0].x == -1){
+            GameOver();
+        }
+        if (snake.body[0].y == cellCount || snake.body[0].y == -1){
+            GameOver();
+        }
+    }
+
+    void checkCollisionWithTail(){
+        deque<Vector2> bodyD = snake.body;
+        bodyD.pop_front();
+        if (elementInDequeu(snake.body[0], bodyD)) GameOver();
+    }
+
+    void GameOver(){
+        snake.Reset();
+        food.position = food.GenerateRandomPos(snake.body);
+        running = false;
     }
 };
 
-class CPUpaddle: public Paddle{
-    public:
-
-    void update(int ball_y){
-        if (y + height/2 > ball_y) y -= speed;
-        if (y + height/2 < ball_y) y += speed;
-        LimitMovement();
-    }
-};
-
-Ball ball;
-Paddle player;
-CPUpaddle cpu;
-
-int main(void){
-    const int width = 640;
-    const int height = 640;
-    InitWindow(width, height, "game");
+int main(){
+    const int width = cellSize*cellCount;
+    const int height = cellSize*cellCount;
+    InitWindow(width, height, "Snake");
     SetTargetFPS(60);
+    Game game;
 
-    ball.radius = 20;
-    ball.x = width/2;
-    ball.y = height/2;
-    ball.speed_x = 7;
-    ball.speed_y = 7;
-
-    player.width = 32;
-    player.height = 128;
-    player.x = width - player.width - 10;
-    player.y = height/2 - player.height/2;
-    player.speed = 6;
-
-    cpu.width = player.width;
-    cpu.height = player.height;
-    cpu.x = 10;
-    cpu.y = height/2 - cpu.height/2;
-    cpu.speed = 6;
 
     while (!WindowShouldClose()){
         BeginDrawing();
 
-        //Updating
-        ball.Update();
-        player.Update();
-        cpu.update(ball.y);
-
-        //Checking for collision
-        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height})){
-            ball.speed_x *= -1;
+        if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1) {
+            game.snake.direction = {0, -1};
+            game.running = true;
         }
-        
-        if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height})){
-            ball.speed_x *= -1;
+        if (IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1) {
+            game.snake.direction = {0, 1};
+            game.running = true;
+        }
+        if (IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1) {
+            game.snake.direction = {1, 0};
+            game.running = true;
+        }
+        if (IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1) {
+            game.snake.direction = {-1, 0};
+            game.running = true;
         }
 
-        // Drawing
-        ClearBackground(Dark_Green);
-        DrawRectangle(width/2, 0, width/2, height, Green);
-        DrawCircle(width/2,height/2, 125, Light_Green);
-        DrawLine(width/2, 0, width/2, height, WHITE);
-        ball.Draw();
-        player.Draw();
-        cpu.Draw();
-        DrawText(TextFormat("%i", cpu_score), width/4, 20, 30, WHITE);
-        DrawText(TextFormat("%i", player_score), 3*width/4, 20, 30, WHITE);
+        if (eventTrigger(0.2)) game.Update();
 
+        ClearBackground(Light_Green);
+        game.Draw();
 
         EndDrawing();
     }
 
-
-    CloseWindow(); 
+    CloseWindow();
     return 0;
 }
